aa8eb3040c1f857cb21d88b9207168a5675ad87f
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-07-13 09:55:11 +0300

    Update bot_for_google.js
    1) Скрипт ищет сайт в поисковой системе на разных страницах поисковой выдачи, вплоть до 10
    2) Скрипт умеет гулять по сайту
    3) С вероятностью 30% скрипт вернёт нас на поисковую систему.

--- bot_for_google.js
+++ bot_for_google.js
@@ -5,12 +5,14 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.google.com/*
+// @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
 // @grant        none
 // ==/UserScript==
 
 let keywords = ["Гобой","Как звучит флейта","Тромбон","Что такое валторна","Фагот","Скрипка","Виолончель"];
 let keyword  = keywords[getRandom(0,keywords.length)];
 let btnK = document.getElementsByName("btnK")[1];
+let links = document.links;
 function getRandom(min,max){
     return Math.floor(Math.random()*(max-min)+min);
 }
@@ -24,17 +26,32 @@ function writeKeyword(word){
             clearInterval(timerId);
             btnK.click();
         }
-    },1000);
+    },100);
 }
 
 if (btnK != undefined)
     writeKeyword(keyword);
-else {
-let links = document.links;
+else if (location.hostname == "www.google.com"){
+    let flag = true;
     for(let i=0; i<links.length; i++){
         if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1){
+            flag=false;
             links[i].click();
             break;
         }
     }
+    if (document.getElementsByClassName("YyVfkd")[0].textContent > 9){
+        flag = false;
+        location.href = "https://www.google.com/";
+    }
+    if (flag) setTimeout(()=>{pnnext.click()},getRandom(3000,6000));
+} else {
+    setInterval(()=>{
+        if (getRandom(0,101)>=70) location.href = "https://www.google.com/";
+        else{
+            let index = getRandom(0,links.length);
+            if(links[index].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1)
+                links[index].click();
+        }
+    },getRandom(2000,6000));
 }
